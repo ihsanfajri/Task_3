23e3b4b70a7cedfa5493f3b1c3f1a2478a0a2223
Gunturf <gunturrf06@gmail.com>
2017-02-20 18:38:33 +0700

    Task 3
    Mohon dikoreksi Captain Fajri Jika Ada yang salah

--- src/task3exercise2/Project.java
+++ src/task3exercise2/Project.java
@@ -4,61 +4,75 @@ package task3exercise2;
 public class Project {
 
     // 1. Declare your private attributes here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154343 
+    // NAMA : Guntur Fatmawan Widodo 
     //
     // Constructor initialize project name, 
     // instantiate array member with size of [5],
     // and set release status = false
     //
     //------------------- your code here-----------------------
+    private String projectName;
+    private Member teamMember[];
+    private int nTeam = 0;
+    private boolean releaseStatus = false;
     
     //---------------------------------------------------------
     
     // 2. Declare your Constructor here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154343 
+    // NAMA : Guntur Fatmawan Widodo 
     //
     //------------------- your code here-----------------------
-    
+    public void project (String projectName) {
+        teamMember = new Member [5];
+        this.releaseStatus = false;
+    }
     //---------------------------------------------------------
     
     
     // 3. Declare your Setter and Getter method here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154343 
+    // NAMA : Guntur Fatmawan Widodo 
     //
     //------------------- your code here-----------------------
-    
+    public boolean isRelease() {
+        return releaseStatus;
+    }
     //---------------------------------------------------------
     
     
     // 4. Declare method addMember( m : Member ) here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154343 
+    // NAMA : Guntur Fatmawan Widodo 
     //
     // add an object Member m into array team member, then
     // increment integer nTeam+1
     //
     //------------------- your code here-----------------------
-    
+    public void addMember (Member m) {
+        this.teamMember[nTeam] = m;
+        nTeam ++;
+    }
     //---------------------------------------------------------
     
     
     // 5. Declare method releaseApp() here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154343 
+    // NAMA : Guntur Fatmawan Widodo 
     //
     // set release status = true
     //
     //------------------- your code here-----------------------
-    
+    public void releaseApp() {
+        releaseStatus = true;
+    }
     //---------------------------------------------------------
     
     
     // 6. Create method toString() : String here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154343 
+    // NAMA : Guntur Fatmawan Widodo 
     //
     // returns String that contains Project name, number of team member, 
     // and release status 
@@ -67,7 +81,13 @@ public class Project {
     //
     public String toString() {
     //------------------- your code here-----------------------
-        return null;
+    if(releaseStatus = true) {
+        System.out.println("Progress");
+    }
+    else
+        System.out.println ("Not Onn Progress");
+    String project = (projectName+ "Status is in"+ releaseStatus+" with team member of"+nTeam);
+    return project;
     
     //---------------------------------------------------------
     }
